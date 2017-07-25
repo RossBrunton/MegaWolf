e3220f31b9e30781b85fd00aa3edd89e18ce77bb
Ross Brunton <bruntonross@gmail.com>
2017-07-25 18:15:11 +0100

    Rest of the add opcodes, and two "and" ones
    Also a bit of bugfixing for other stuff that exists already.

--- emulator.js
+++ emulator.js
@@ -1,15 +1,15 @@
 "use strict";
-const SP = 15;
-const PC = 16;
-const CCR = 17;
-const DBASE = 0;
-const ABASE = 8;
+export const SP = 15;
+export const PC = 16;
+export const CCR = 17;
+export const DBASE = 0;
+export const ABASE = 8;
 
-const C = 0x0001; // Carry
-const V = 0x0002; // Overflow
-const Z = 0x0004; // Zero
-const N = 0x0008; // Negative
-const X = 0x0010; // Extend
+export const C = 0x0001; // Carry
+export const V = 0x0002; // Overflow
+export const Z = 0x0004; // Zero
+export const N = 0x0008; // Negative
+export const X = 0x0010; // Extend
 
 let u8 = new Uint8Array(1);
 let u16 = new Uint16Array(1);
@@ -85,9 +85,28 @@ let makeSigned = function(val, length) {
     }
 }
 
+let addCcr = function(a, b, result) {
+    let ccr = 0;
+    ccr |= tmp[0] == 0 ? Z : 0;
+    ccr |= isNegative(tmp[0], length) ? N : 0;
+    if(tmp[0] < reg) {
+        ccr |= C | X;
+    }
+    if(ea & reg & (1 << ((length * 8) - 1)) || ~ea & ~reg & (1 << ((length * 8) - 1))) {
+        if(!(ea & tmp[0] & (1 << ((length * 8) - 1)))) {
+            ccr |= V;
+        }
+    }
+    return ccr;
+}
+
 export class Emulator {    
     constructor(options) {
-        this.options = options;
+        this.options = options ? options : {};
+        if(this.options.version === undefined) {
+            this.options.version = 0xa1a1;
+        }
+        
         
         this.mainRam = new DataView(new ArrayBuffer(1024*64));
         this.registers = new Uint32Array(18);
@@ -101,16 +120,24 @@ export class Emulator {
     readMemory(addr) {
         console.log("Memory read at 0x"+addr.toString(16));
         
-        if(addr < 0x003fffff) {
+        if(addr < 0x3fffff) {
             // ROM
             return this.rom.getUint16(addr, false);;
         }
+        
+        if(addr == 0xa10000 || addr == 0xa10001) {
+            return this.options.version;
+        }
+        
+        console.warn("Read from unknown memory address 0x"+addr.toString(16));
         
         return 0;
     }
     
     writeMemory(addr, value) {
         console.log("Memory write at 0x"+addr.toString(16)+", value 0x"+value.toString(16));
+        
+        console.warn("Write to unknown memory address 0x"+addr.toString(16));
     }
     
     readMemory8(addr) {
@@ -251,6 +278,24 @@ export class Emulator {
         }
     }
     
+    getImmediate(instruction) {
+        let immediate;
+        switch(instruction & 0x00c0) {
+            case 0x0000:
+                immediate = this.readMemory8(this.registers[PC] + 1);
+                this.registers[PC] += 2;
+                return [1, immediate, u8];
+            case 0x0040:
+                immediate = this.readMemory(this.registers[PC]);
+                this.registers[PC] += 2;
+                return [2, immediate, u16];
+            case 0x0080:
+                immediate = this.readMemory32(this.registers[PC]);
+                this.registers[PC] += 4;
+                return [4, immediate, u32];
+        }
+    }
+    
     start() {
         this.registers[SP] = this.readMemory32(0x0000);
         this.registers[PC] = this.readMemory32(0x0004);
@@ -271,6 +316,7 @@ export class Emulator {
             case 0x4a40:
             case 0x4a80: {
                 // tst
+                console.log("> tst");
                 let length = 1;
                 if(noEffectiveAddress == 0x4a40) {
                     length = 2;
@@ -290,6 +336,7 @@ export class Emulator {
             case 0x4c80:
             case 0x4cc0: {
                 // movem (mem to register)
+                console.log("> movem (to registers)");
                 let length = 2;
                 if(noEffectiveAddress == 0x4cc0) {
                     length = 4;
@@ -309,12 +356,14 @@ export class Emulator {
         
         if((instruction & 0xf1f0) == 0xc100) {
             // abcd
+            console.log("> abcd");
             console.error("ABCD opcode not yet supported.");
-            return true;
+            return false;
         }
         
-        if((instruction & 0xf000) == 0xd0000) {
+        if((instruction & 0xf000) == 0xd000) {
             // add/adda
+            console.log("> add/adda");
             let register = (instruction >> 9) & 0b111;
             let opmode = (instruction >> 6) & 0b111;
             let length = 0;
@@ -351,7 +400,6 @@ export class Emulator {
                 let ea = makeSigned(this.readEa(effectiveAddress, length));
                 
                 this.registers[register] += ea;
-                break;
             }else{
                 // < ea > + Dn -> Dn / < ea >
                 let eaAddr = 0;
@@ -367,18 +415,7 @@ export class Emulator {
                 tmp[0] = ea;
                 tmp[0] += reg;
                 
-                let ccr = 0;
-                ccr |= tmp[0] == 0 ? Z : 0;
-                ccr |= isNegative(tmp[0], length) ? N : 0;
-                if(tmp[0] < reg) {
-                    ccr |= C | X;
-                }
-                if(ea & reg & (1 << ((length * 8) - 1)) || ~ea & ~reg & (1 << ((length * 8) - 1))) {
-                    if(!(ea & tmp[0] & (1 << ((length * 8) - 1)))) {
-                        ccr |= V;
-                    }
-                }
-                this.registers[CCR] = ccr;
+                this.registers[CCR] = addCcr(ea, reg, tmp[0]);
                 
                 if(opmode & 0b100) {
                     this.writeMemoryN(eaAddr, tmp[0], length);
@@ -390,8 +427,174 @@ export class Emulator {
             return true;
         }
         
+        if((instruction & 0xff00) == 0x0600 || (instruction & 0xf100) == 0x5000) {
+            // addi
+            console.log("> addi/addq");
+            let length = 0;
+            let immediate = 0;
+            let tmp;
+            let q = (instruction & 0xf100) == 0x5000;
+            switch(instruction & 0x00c0) {
+                case 0x0000:
+                    length = 1;
+                    if(!q) {
+                        immediate = this.readMemory8(this.registers[PC] + 1);
+                        this.registers[PC] += 2;
+                    }
+                    tmp = u8;
+                    break;
+                case 0x0040:
+                    length = 2;
+                    if(!q) {
+                        immediate = this.readMemory(this.registers[PC]);
+                        this.registers[PC] += 2;
+                    }
+                    tmp = u16;
+                    break;
+                case 0x0080:
+                    length = 4;
+                    if(!q) {
+                        immediate = this.readMemory32(this.registers[PC]);
+                        this.registers[PC] += 4;
+                    }
+                    tmp = u32;
+                    break;
+            }
+            
+            if(q) {
+                immediate = (instruction >> 9) & 0b111;
+                if(immediate == 0) immediate = 8;
+            }
+            
+            if((effectiveAddress & 0b111000) == 0b000000) {
+                // To data register
+                let reg = this.registers[effectiveAddress];
+                tmp[0] = reg;
+                tmp[0] += immediate;
+                
+                this.registers[CCR] = addCcr(immediate, reg, tmp[0]);
+                
+                this.registers[effectiveAddress] = tmp[0];
+            }else if((effectiveAddress & 0b111000) == 0b001000) {
+                // To address register
+                if(!q) {
+                    console.log("Tried to add to an address register!");
+                    return false;
+                }
+                this.registers[effectiveAddress] += immediate;
+            }else{
+                // To memory address
+                let addr = this.addressEa(effectiveAddress);
+                let ea = this.readMemoryN(addr, length);
+                tmp[0] = ea;
+                tmp[0] += immediate;
+                
+                this.registers[CCR] = addCcr(immediate, ea, tmp[0]);
+                
+                this.writeMemoryN(addr, tmp[0], length);
+            }
+            
+            return true;
+        }
+        
+        if((instruction & 0xf130) == 0xd100) {
+            // addx
+            console.log("> addx");
+            console.error("ADDX opcode not yet supported.");
+            return false;
+        }
+        
+        if((instruction & 0xf000) == 0xc000) {
+            // and
+            console.log("> and");
+            let register = (instruction >> 9) & 0b111;
+            let opmode = (instruction >> 6) & 0b111;
+            let length = 0;
+            let tmp;
+            
+            // Get length
+            switch(opmode) {
+                case 0b000:
+                case 0b100:
+                    length = 1;
+                    tmp = u8;
+                    break;
+                
+                case 0b001:
+                case 0b101:
+                    length = 2;
+                    tmp = u16;
+                    break;
+                
+                case 0b010:
+                case 0b110:
+                    length = 4;
+                    tmp = u32;
+                    break;
+            }
+            
+            // Do the math
+            let eaAddr = 0;
+            let ea = 0;
+            if(opmode & 0b100) {
+                eaAddr = this.addressEa(effectiveAddress, length);
+                ea = this.readMemory(eaAddr);
+            }else{
+                ea = this.readEa(effectiveAddress, length);
+            }
+            
+            let reg = this.registers[register];
+            tmp[0] = ea;
+            tmp[0] &= reg;
+            
+            let ccr = this.registers[CCR] & X;
+            ccr |= (tmp[0] & (1 << ((length * 8) - 1))) ? N : 0;
+            ccr |= tmp[0] == 0 ? Z : 0;
+            this.registers[CCR] = ccr;
+            
+            if(opmode & 0b100) {
+                this.writeMemoryN(eaAddr, tmp[0], length);
+            }else{
+                this.registers[register] = tmp[0];
+            }
+            
+            return true;
+        }
+        
+        if((instruction & 0xff00) == 0x0200) {
+            // andi
+            console.log("> andi");
+            let [length, immediate, tmp] = this.getImmediate(instruction);
+            
+            if((effectiveAddress & 0b111000) == 0b000000) {
+                // To data register
+                let reg = this.registers[effectiveAddress];
+                this.registers[effectiveAddress] &= immediate;
+                
+                let ccr = this.registers[CCR] & X;
+                ccr |= (this.registers[effectiveAddress]) & (1 << ((length * 8) - 1)) ? N : 0;
+                ccr |= (this.registers[effectiveAddress]) == 0 ? Z : 0;
+                this.registers[CCR] = ccr;
+            }else{
+                // To memory address
+                let addr = this.addressEa(effectiveAddress);
+                let ea = this.readMemoryN(addr, length);
+                tmp[0] = ea;
+                tmp[0] &= immediate;
+                
+                let ccr = this.registers[CCR] & X;
+                ccr |= (this.registers[effectiveAddress]) & (1 << ((length * 8) - 1)) ? N : 0;
+                ccr |= (this.registers[effectiveAddress]) == 0 ? Z : 0;
+                
+                this.writeMemoryN(addr, tmp[0], length);
+            }
+            
+            return true;
+        }
+        
         if((instruction & 0xf1c0) == 0x41c0) {
             // lea
+            console.log("> lea");
             // TODO: Use only supported modes
             let reg = (instruction & 0x0e00) >> 9;
             this.registers[ABASE + reg] = this.addressEa(effectiveAddress, 4);
@@ -400,7 +603,8 @@ export class Emulator {
         
         if((instruction & 0xf000) == 0x6000) {
             // bcc
-            let condition = instruction & 0x0f00;
+            console.log("> bcc");
+            let condition = (instruction & 0x0f00) >> 8;
             let displacement = instruction & 0x00ff;
             
             if(doCondition(condition, this.registers[CCR])) {
@@ -412,13 +616,14 @@ export class Emulator {
                     displacement = makeSigned(displacement, 1);
                 }
                 
-                this.registers[PC] = oldPc + displacement;
+                this.registers[PC] = oldPc + displacement + 2;
             }
             return true;
         }
         
         if((instruction & 0xc000) == 0x0000) {
             // move/mavea
+            console.log("> move/movea");
             let length = 1;
             if((instruction & 0x3000) == 0x3000) {
                 length = 2;
---
+++ tests.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import * from "./emulator.js";
+
+let tests = [
+    
+]
+
+export runTests() {
+    for(t of tests) {
+        console.log(t);
+    }
+}
