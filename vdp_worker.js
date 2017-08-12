015136576b37ba764491e48a458c3f0fc9b26182
Ross Brunton <bruntonross@gmail.com>
2017-08-12 21:02:15 +0100

    Horizontal scrolling now working properly in vdp

--- vdp_worker.js
+++ vdp_worker.js
@@ -107,6 +107,7 @@ let getPlaneRows = function() {
 }
 
 let getHScroll = function(plane, line) {
+    // TODO: Each individual scanline has its own offest, not the whole tile
     if(plane == S) return 0;
     let base = registers[RHORSCROLL] << 10;
     
@@ -115,7 +116,7 @@ let getHScroll = function(plane, line) {
             return vram.getUint16(base + (plane * 2), false);
             
         case HSCROLL_TILE:
-            return vram.getUint16(base + ((line / 8) * 4) + (plane * 2), false);
+            return vram.getUint16(base + (~~(line / 8) * 4) + (plane * 2), false);
         
         case HSCROLL_SCANLINE:
             return vram.getUint16(base + (line * 4) + (plane * 2), false);
@@ -138,6 +139,22 @@ let raf = function() {
     if(registers[RM2] & 0x20) {
         registers[REX] = 6;
     }
+}
+
+// Gets a pointer to a cell for a given x, y cell on the given plane
+let getFromPlane = function(x, y, plane) {
+    let base = 0;
+    if(plane == A) {
+        base = (registers[RPLANEA_NT] & 0x38) << 10;
+    }else{
+        base = (registers[RPLANEB_NT] & 0x07) << 13;
+    }
+    
+    while(x > pcols) x -= pcols;
+    while(x < 0) x += pcols;
+    
+    let offset = ((y * pcols) + x) * 2;
+    return base + offset;
 }
 
 // Converts the colour into a 0xrrggbbaa 32 bit int
@@ -224,11 +241,14 @@ let doFrame = function() {
 let drawPlane = function(start, priority, view, plane) {
     for(let y = 0; y < prows; y ++) {
         for(let x = 0; x < pcols; x ++) {
-            let cell = vram.getUint16(start, false);
-            start += 2;
+            let scrollx = -getHScroll(plane, y);
+            let tx = scrollx >> 3;
+            let txrem = (scrollx & 0b111) * (scrollx > 0 ? 1 : -1);
+            
+            let cell = vram.getUint16(getFromPlane(x + tx, y, plane), false);
             if(((cell & 0x8000) == 0x8000) == priority) {
                 // Priority is correct
-                drawTile(cell, x * 8, y * 8, view, plane);
+                drawTile(cell, x * 8 + txrem, y * 8, view, plane);
             }
         }
     }
@@ -272,9 +292,6 @@ let drawTile = function(cell, x, y, view, plane) {
     
     for(let ys = 0; ys < 8; ys ++) {
         for(let xs = 0; xs < 8; xs ++) {
-            let xput = xs + getHScroll(plane, ybase + ys);
-            let yput = ys;
-            
             let value;
             if(hi) {
                 value = vram.getUint8(i) >>> 4;
